ffc1eea50516ac0a4bac37aed9ae84c99cfc2ca0
anmolsingh7275 <anmolprogramming@gmail.com>
2025-08-15 16:46:28 +0530

    second commit

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ const io =  new Server(server,{
     },
 
 });
+const rooms = new  Map()
 
 io.on("connection",(socket) => {
     console.log("User Connected", socket.id);
+
+    let currentRoom = null;
+    let currentUser = null;
+    
+    socket.on("join",({roomId, UserName}) =>{
+
+        if(currentRoom){
+            socket.leave(currentRoom);
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit("UserJoined",Array.from(rooms.get(currentRoom)));
+        }
+        currentRoom = roomId;
+        currentUser= UserName;
+        socket.join(roomId);
+
+        if(!rooms.has(roomId)){
+            rooms.set(roomId, new Set());
+        }
+        rooms.get(roomId).add(UserName)
+
+        io.to(roomId).emit("UserJoined",Array.from(rooms.get(currentRoom)));
+    });
 });
 
 const port = process.env.PORT || 5000;
