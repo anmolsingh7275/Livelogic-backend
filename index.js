a10cdd5a1b85a84e1477c6cedde3f2599e2f6b70
anmolsingh7275 <anmolprogramming@gmail.com>
2025-08-23 02:28:49 +0530

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io =  new Server(server,{
     },
 
 });
-const rooms new  = new  Map()
+const rooms = new  Map()
 
 io.on("connection",(socket) => {
     console.log("User Connected", socket.id);
@@ -23,7 +23,7 @@ io.on("connection",(socket) => {
         if(currentRoom){
             socket.leave(currentRoom);
             rooms.get(currentRoom).delete(currentUser);
-            io.to(currentRoom).emit("UserJoined",Array.from(rooms.get(currentRoom)));
+            io.to(currentRoom).emit("userJoined",Array.from(rooms.get(currentRoom)));
         }
         currentRoom = roomId;
         currentUser= userName;
@@ -34,12 +34,30 @@ io.on("connection",(socket) => {
         }
         rooms.get(roomId).add(userName)
 
-        io.to(roomId).emit("UserJoined",Array.from(rooms.get(currentRoom)));
-        console.log("User Joined room ", roomId)
+        io.to(roomId).emit("userJoined",Array.from(rooms.get(currentRoom)));
+        
     });
     socket.on("codeChange",({roomId, code})  =>{
         socket.to(roomId).emit("codeUpdate",code);
     });
+
+    socket.on("leaveRoom",()=>{
+        if(currentRoom && currentUser){
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit("userJoined",Array.from(rooms.get(currentRoom)));
+        socket.leave(currentRoom)
+        currentRoom =  null;
+        currentUser = null;
+        } 
+    });
+    socket.on("disconnect",()=>{
+        if(currentRoom && currentUser){
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit("userJoined",Array.from(rooms.get(currentRoom)));
+        
+        }
+        console.log("user Disconnected");
+    })
 });
 
 const port = process.env.PORT || 5000;
